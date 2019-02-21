1adeacfcb7326cb7c7780a09c1b215527db72725
Sergio Belda <serbelga@inf.upv.es>
2019-02-21 23:01:38 +0100

    Android Navigation -> KTX Navigation

--- app/src/main/java/com/example/sergiobelda/gramophone/ui/mainmenu/pages/AlbumsAdapter.kt
+++ app/src/main/java/com/example/sergiobelda/gramophone/ui/mainmenu/pages/AlbumsAdapter.kt
@@ -3,8 +3,10 @@ package com.example.sergiobelda.gramophone.ui.mainmenu.pages
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
-import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sergiobelda.gramophone.R
 import com.example.sergiobelda.gramophone.util.Album
@@ -29,11 +31,15 @@ class AlbumsAdapter(private var albums: ArrayList<Album>) : RecyclerView.Adapter
         private var albumNameTextView : TextView = itemView.findViewById(R.id.albumNameTextView)
         private var artistTextView : TextView = itemView.findViewById(R.id.artistTextView)
         private var albumCardView : MaterialCardView = itemView.findViewById(R.id.albumCardView)
+        private var albumImageView : ImageView = itemView.findViewById(R.id.albumImageView)
 
         fun bindAlbum(album: Album) {
             albumNameTextView.text = album.name
+            /*
+            val extras = FragmentNavigatorExtras(
+                albumImageView to "detail_image")*/
             albumCardView.setOnClickListener {
-                findNavController(it).navigate(R.id.action_menuFragment_to_albumDetailFragment)
+                it.findNavController().navigate(R.id.action_menuFragment_to_albumDetailFragment)
             }
         }
     }
