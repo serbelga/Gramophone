bf436e7a44e37e530341c5800b8afecfbb321211
Sergio Belda <serbelga@inf.upv.es>
2019-04-30 02:28:54 +0200

    Add BottomSheet Player and Modified Settings

--- app/src/main/java/com/example/sergiobelda/gramophone/ui/preferences/SettingsActivity.kt
+++ app/src/main/java/com/example/sergiobelda/gramophone/ui/preferences/SettingsActivity.kt
@@ -2,23 +2,27 @@ package com.example.sergiobelda.gramophone.ui.preferences
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.fragment.app.commit
-import androidx.fragment.app.transaction
 import com.example.sergiobelda.gramophone.R
+import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-        /*
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.settings, SettingsFragment())
-            .commit()
-        */
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportFragmentManager.commit {
             replace(R.id.settings, SettingsFragment(), null)
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> onBackPressed()
+        }
+        return true
     }
 }
--- app/src/main/java/com/example/sergiobelda/gramophone/ui/preferences/SettingsFragment.kt
+++ app/src/main/java/com/example/sergiobelda/gramophone/ui/preferences/SettingsFragment.kt
@@ -2,9 +2,6 @@ package com.example.sergiobelda.gramophone.ui.preferences
 
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.preference.PreferenceFragmentCompat
 
 import com.example.sergiobelda.gramophone.R
