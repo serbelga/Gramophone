8e1b22212da47dbe332c3a0f030bd9f0b7bf0bb0
Sergio Belda Galbis <serbelga@inf.upv.es>
2019-11-03 15:19:55 +0100

    Add mode_night preference to Settings, add recyclerview dependency to fix preference-ktx crash

--- app/src/main/java/com/android/sergiobelda/gramophone/ui/preferences/SettingsActivity.kt
+++ app/src/main/java/com/android/sergiobelda/gramophone/ui/preferences/SettingsActivity.kt
@@ -2,14 +2,15 @@ package com.android.sergiobelda.gramophone.ui.preferences
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.MenuItem
 import androidx.fragment.app.commit
-import androidx.preference.Preference
-import androidx.preference.PreferenceManager
 import com.android.sergiobelda.gramophone.R
 import kotlinx.android.synthetic.main.activity_settings.*
 
+/**
+ * SettingsActivity
+ * @author Sergio Belda Galbis (@serbelga)
+ */
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/android/sergiobelda/gramophone/ui/preferences/SettingsFragment.kt
+++ app/src/main/java/com/android/sergiobelda/gramophone/ui/preferences/SettingsFragment.kt
@@ -3,24 +3,39 @@ package com.android.sergiobelda.gramophone.ui.preferences
 
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
-import androidx.preference.Preference
+import androidx.appcompat.app.AppCompatDelegate.*
 import androidx.preference.PreferenceFragmentCompat
 
 import com.android.sergiobelda.gramophone.R
 
 /**
- *
- *
+ * SettingsFragment
+ * @author Sergio Belda Galbis (@serbelga)
  */
 class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.pref_main, rootKey)
+        preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        preferenceManager.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
     }
 
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
-        if (key == "theme") {
-            Log.i(TAG, "Preference value was updated to: " + sharedPreferences!!.getString(key, ""))
+        if (key == "mode_night") {
+            val modeNight = sharedPreferences!!.getBoolean(key, true)
+            if (modeNight) {
+                setDefaultNightMode(MODE_NIGHT_YES)
+            } else {
+                setDefaultNightMode(MODE_NIGHT_NO)
+            }
         }
     }
 
